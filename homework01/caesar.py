fbd774bbe44bee7c5d56605a930582fec752672e
Баранова Софья <sofiaba2007@gmail.com>
2025-10-30 11:50:48 +0300

    add file

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -30,11 +30,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
     return ciphertext
 
-
-
-
-
-
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
