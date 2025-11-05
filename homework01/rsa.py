97f25965660dea665192b6c70f9256d7310a9ec7
Баранова Софья <sofiaba2007@gmail.com>
2025-11-05 14:33:02 +0300

    add file

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -74,9 +74,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     return old_coef
 
 
-def generate_keypair(
-    p: int, q: int
-) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     if p == q:
