d7342693101013352bad018256f3fe0fafae811a
Баранова Софья <sofiaba2007@gmail.com>
2025-11-05 13:36:48 +0300

    add file

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -47,7 +47,7 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
                 basechar = "A"
             else:
                 basechar = "a"
-            shifted_char = chr(ord(basechar)+ ((ord(char) - ord(basechar) - shift) % amount_of_letters))
+            shifted_char = chr(ord(basechar) + ((ord(char) - ord(basechar) - shift) % amount_of_letters))
             plaintext += shifted_char
 
         else:
