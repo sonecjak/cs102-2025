135abf06bfbe1ebaa8fe7de9eef6159291d6d5e3
Баранова Софья <sofiaba2007@gmail.com>
2025-12-25 18:58:56 +0300

    add file

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -12,15 +12,16 @@ def draw_cell(x, y, color, size: int = 10):
     canvas.create_rectangle(x, y, x1, y1, fill=color)
 
 
-def draw_maze(grid: List[List[str]], size: int = 10):
+def draw_maze(grid: List[List[str | int]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                color = "White"
             elif cell == "■":
-                color = 'black'
+                color = "black"
             elif cell == "X":
                 color = "red"
+                color = "green"
             draw_cell(y, x, color, size)
 
 
@@ -38,10 +39,13 @@ if __name__ == "__main__":
     N, M = 51, 77
 
     CELL_SIZE = 10
-    GRID = bin_tree_maze(N, M)
+    trying_grid = bin_tree_maze(N, M)
+    while not solve_maze(deepcopy(trying_grid))[1]:
+        try_grid = bin_tree_maze(N, M)
+    GRID = trying_grid
 
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
@@ -51,4 +55,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
